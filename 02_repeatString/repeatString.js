4981344f94f57a90f0a38f9f5e6dcbbdb8f1ec6d
MAD-GOD01 <ac9949044@gmail.com>
2024-05-29 09:20:24 +0545

    Complete exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let result = "";
+    if(times>=0){
+        for(let i=0; i<times; i++){
+            result = result + word;
+        }
+    }
+    else
+        result = "ERROR";
+    return result;
 };
 
 // Do not edit below this line
